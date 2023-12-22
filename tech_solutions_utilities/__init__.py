ab5de307794f68fd35b1e9952d154b3ddff6e070
Michele Maestrale <michele.maestrale@glovoapp.com>
2023-12-22 10:30:38 +0100

    modified init with changed names modules

--- tech_solutions_utilities/__init__.py
+++ tech_solutions_utilities/__init__.py
@@ -1,3 +1,3 @@
-from .mysql_database import MySQLDatabase
-from .sftp_client import SFTPClient
+from .mysql_database_manager import MySQLDatabase
+from .sftp_client_manager import SFTPClient
 from .trino_manager import TrinoManager
